eb69bfdf53e1a1cab78e1cfd493d96b13b09879f
4ndrisw <4ndr1sw@gmail.com>
2023-05-06 21:47:06 +0700

    Update quotations_preview_template.php

--- views/admin/quotations/quotations_preview_template.php
+++ views/admin/quotations/quotations_preview_template.php
@@ -99,7 +99,7 @@
             <a href="<?php echo admin_url('quotations/quotation/'.$quotation->id); ?>" data-placement="left" data-toggle="tooltip" title="<?php echo _l('quotation_edit'); ?>" class="btn btn-default btn-with-tooltip" data-placement="bottom"><i class="fa fa-pencil-square"></i></a>
             <?php } ?>
             <div class="btn-group">
-               <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-file-pdf"></i><?php if(is_mobile()){echo ' PDF';} ?> <span class="caret"></span></a>
+               <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-file-pdf-o"></i><?php if(is_mobile()){echo ' PDF';} ?> <span class="caret"></span></a>
                <ul class="dropdown-menu dropdown-menu-right">
                   <li class="hidden-xs"><a href="<?php echo site_url('quotations/pdf/'.$quotation->id.'?output_type=I'); ?>"><?php echo _l('view_pdf'); ?></a></li>
                   <li class="hidden-xs"><a href="<?php echo site_url('quotations/pdf/'.$quotation->id.'?output_type=I'); ?>" target="_blank"><?php echo _l('view_pdf_in_new_window'); ?></a></li>
